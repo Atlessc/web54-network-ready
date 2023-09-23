b53e419a1a11cf91074018dd99fcff0cde8eb089
Tyler Smith <tylersmith4596@gmail.com>
2023-09-22 20:31:58 -0700

    cleaned up the code to make the nodes smaller

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from 'react';
 import { saveAs } from 'file-saver';
 import './App.css';
 import BackgroungImg from './component/background.jsx';
-import { Link } from 'react-router-dom';
 
 function App() {
-  const node1 = "-550"
-  const node2 = "50"
-  const node3 = "650"
+  const node1 = "-400"
+  const node2 = "40"
+  const node3 = "500"
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
   const [vCardData, setVCardData] = useState('');
   const [activeNode, setActiveNode] = useState(node1);
   const [cardInfo, setCardInfo] = useState('Web Dev');
-  const [cardRelative2NodePosition, setCardRelative2NodePosition] = useState('0px');
-  const [triRelative2NodePosition, setTriRelative2NodePosition] = useState('-157px');
+
+  const [cardRelative2NodePosition, setCardRelative2NodePosition] = useState('40px');
+  const [triRelative2NodePosition, setTriRelative2NodePosition] = useState('-160px');
 
   useEffect(() => {
     fetch('/ContactInfo.vcf')
@@ -33,8 +36,8 @@ function App() {
 const setNodeto1 = () => {
   setActiveNode(node1)
   setCardInfo('Web Dev')
-  setCardRelative2NodePosition('0px')
-  setTriRelative2NodePosition('-157px')
+  setCardRelative2NodePosition('40px')
+  setTriRelative2NodePosition('-160px')
 }
 
 const setNodeto2 = () => {
@@ -47,7 +50,7 @@ const setNodeto2 = () => {
 const setNodeto3 = () => {
   setActiveNode(node3)
   setCardInfo('Music')
-  setCardRelative2NodePosition('250px')
+  setCardRelative2NodePosition('210px')
   setTriRelative2NodePosition('-87px')
 }
 
@@ -63,30 +66,30 @@ const setNodeto3 = () => {
         </div>
       </div>
       <div className="node-animation">
-          <svg height={80} width={400} viewBox='0 0 80 300' preserveAspectRatio="xMidYMid">
-            <circle className='Active' cx={activeNode} cy="150" r="50" />
-            <circle className='node 1' cx="-550" cy="150" r="100" onClick={setNodeto1}/>
-            <circle className='node 2' cx="50" cy="150" r="100" onClick={setNodeto2} />
-            <circle className='node 3'cx="650" cy="150" r="100" onClick={setNodeto3} />
-            <line x1="125" y1="150" x2="550" y2="150"/>
-            <line x1="-450" y1="150" x2="-50" y2="150"/>
+          <svg height={80} width={300} viewBox='0 0 80 300' preserveAspectRatio="xMidYMid">
+            <circle className='Active' cx={activeNode} cy="150" r="40" />
+            <circle className='node 1' cx="-400" cy="150" r="80" onClick={setNodeto1}/>
+            <circle className='node 2' cx="40" cy="150" r="80" onClick={setNodeto2} />
+            <circle className='node 3'cx="500" cy="150" r="80" onClick={setNodeto3} />
+            <line x1="130" y1="150" x2="425" y2="150"/>
+            <line x1="-315" y1="150" x2="-40" y2="150"/>
           </svg>
-          <div className="card-container" style={{position: 'relative', left: `${cardRelative2NodePosition}`, transition:'left 650ms ease-in-out'}}>
-      <svg width={25} height={25} style={{position: 'relative', left: `${triRelative2NodePosition}`, transition:'left 650ms ease-in-out'}}>
-        <polygon points="12.5,7 0,25 25,25" fill="white" className='triangle'/>
-      </svg>
-        <div className="box">
-          {cardInfo}
-          <br/>
-          {cardInfo === 'Web Dev' && <a href='https://web54.dev' target='_blank' rel='noopener noreferrer'>Web54.Dev</a>}
-          {cardInfo === 'Resume' && <a href='https://tysmith.one' target='_blank' rel='noopener noreferrer'>TySmith.One</a>}
-          {cardInfo === 'Music' && 
-          <div className='link-stack'>
-          <a href='https://www.youtube.com/@atlessc5286' target='_blank' rel='noopener noreferrer'>YouTube</a>
-          <a href='https://music.apple.com/us/artist/atlessc/1385247600' target='_blank' rel='noopener noreferrer'>Apple Music</a>
-          </div>}
+        <div className="card-container" style={{position: 'relative', left: `${cardRelative2NodePosition}`, transition:'left 650ms ease-in-out'}}>
+          <svg width={25} height={25} style={{position: 'relative', left: `${triRelative2NodePosition}`, transition:'left 650ms ease-in-out'}}>
+            <polygon points="12.5,7 0,25 25,25" fill="white" className='triangle'/>
+          </svg>
+          <div className="box">
+            {cardInfo}
+            <br/>
+            {cardInfo === 'Web Dev' && <a href='https://web54.dev' target='_blank' rel='noopener noreferrer'>Web54.Dev</a>}
+            {cardInfo === 'Resume' && <a href='https://tysmith.one' target='_blank' rel='noopener noreferrer'>TySmith.One</a>}
+            {cardInfo === 'Music' &&
+            <div className='link-stack'>
+            <a href='https://www.youtube.com/@atlessc5286' target='_blank' rel='noopener noreferrer'>YouTube</a>
+            <a href='https://music.apple.com/us/artist/atlessc/1385247600' target='_blank' rel='noopener noreferrer'>Apple Music</a>
+            </div>}
+          </div>
         </div>
-      </div>
       </div>
 
       <div className='CTA-Button'>
